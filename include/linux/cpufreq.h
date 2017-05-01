5dc08cd17df45660c665cd616119aeb16a54324f
Abhishek <basevd94@gmail.com>
2017-05-01 19:39:21 +0530

    Added SmartAss2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -399,6 +399,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_THUNDERX)
 extern struct cpufreq_governor cpufreq_gov_thunderx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_thunderx)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
